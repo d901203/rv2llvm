74a120137a597e521cc08105dbc79dffbb8fedb1
rv <you@example.com>
2023-01-05 01:26:13 +0800

    update

--- go.mod
+++ go.mod
@@ -2,12 +2,18 @@ module rv2llvm
 
 go 1.19
 
-require github.com/llir/llvm v0.3.6
+require (
+	github.com/llir/llvm v0.3.6
+	github.com/urfave/cli/v2 v2.23.7
+)
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/mewmew/float v0.0.0-20211212214546-4fe539893335 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/mod v0.7.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/tools v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
 func main() {
-	path := "./tests/qrcode.riscv"
-	elf := NewELF(path)
+	var file string
+	app := &cli.App{
+		Name:      "rv2llvmSBT",
+		Usage:     "a tool to translate RISC-V ELF to LLVM IR.",
+		UsageText: "rv2llvmSBT [filename]",
+		Action: func(c *cli.Context) error {
+			if c.NArg() == 0 {
+				log.Fatal("No input file, use -h to see help.")
+			}
+			file = c.Args().Get(0)
+			return nil
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+	elf := NewELF(file)
 	builder := NewIrBuilder(elf)
 	builder.BuildIr(elf.Insts)
 	builder.SetAddrBlk()
